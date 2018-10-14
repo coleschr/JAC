5698344ec7fdaeb43f910d48f5f78e96d35087b8
cole <coleschr@usc.edu>
2018-10-14 13:33:18 -0700

    final touches

--- app/src/main/java/com/example/cole/jac/FragmentHome.java
+++ app/src/main/java/com/example/cole/jac/FragmentHome.java
@@ -8,10 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class FragmentHome extends Fragment implements View.OnClickListener {
     private ImageButton add;
+    private ImageView doc1, doc11;
+    private TextView doc11text;
     private Fragment currentFragment;
 
     @Nullable
@@ -25,7 +30,13 @@ public class FragmentHome extends Fragment implements View.OnClickListener {
         setOnClickListeners();
 
         //get any other initial set up done
-        ((MainActivity)this.getActivity()).screen = 0;
+        if(((MainActivity)this.getActivity()).screen == 2
+                || ((MainActivity)this.getActivity()).screen == 1)
+        {
+            doc11.setVisibility(View.VISIBLE);
+            doc11text.setVisibility(View.VISIBLE);
+        }
+
 
         //return the view that we inflated
         return rootView;
@@ -33,10 +44,15 @@ public class FragmentHome extends Fragment implements View.OnClickListener {
 
     private void wireWidgets(View rootView) {
         add = rootView.findViewById(R.id.button_add);
+        doc1 = rootView.findViewById(R.id.imageView1);
+        doc11 = rootView.findViewById(R.id.imageView11);
+        doc11text = rootView.findViewById(R.id.textView11);
     }
 
     private void setOnClickListeners() {
         add.setOnClickListener(this);
+        doc1.setOnClickListener(this);
+        doc11.setOnClickListener(this);
     }
 
     @Override
@@ -45,10 +61,26 @@ public class FragmentHome extends Fragment implements View.OnClickListener {
             case R.id.button_add:
                 currentFragment = new FragmentEdit();
                 switchToNewScreen();
+            case R.id.imageView1:
+                if(((MainActivity)this.getActivity()).screen == 2
+                        || ((MainActivity)this.getActivity()).screen == 1)
+                {
+                    currentFragment = new FragmentImage();
+                    switchToNewScreen();
+                }
+            case R.id.imageView11:
+                if(((MainActivity)this.getActivity()).screen == 2
+                        || ((MainActivity)this.getActivity()).screen == 1)
+                {
+                    currentFragment = new FragmentImage();
+                    switchToNewScreen();
+                }
+
         }
     }
 
     private void switchToNewScreen() {
+        ((MainActivity)this.getActivity()).screen = 0;
         //tell the fragment manager that if our current fragment isn't null, to replace whatever is there with it
         FragmentManager fm = getFragmentManager();
         if (currentFragment != null) {
--- app/src/main/java/com/example/cole/jac/FragmentImage.java
+++ app/src/main/java/com/example/cole/jac/FragmentImage.java
@@ -1,24 +1,16 @@
 package com.example.cole.jac;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.Camera;
 import android.hardware.camera2.CameraManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import java.io.IOException;
-
-public class FragmentCamera extends Fragment implements View.OnClickListener {
+public class FragmentImage extends Fragment implements View.OnClickListener {
     private ImageView capture, camera_image;
     private Fragment currentFragment;
 
@@ -26,7 +18,7 @@ public class FragmentCamera extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View rootView = inflater.inflate(R.layout.camera_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.image_fragment, container, false);
 
         //wire any widgets -- must use rootView.findViewById
         wireWidgets(rootView);
@@ -55,9 +47,7 @@ public class FragmentCamera extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.imageView_capture:
-
-
-                currentFragment = new FragmentEdit();
+                currentFragment = new FragmentHome();
                 switchToNewScreen();
                 break;
         }
