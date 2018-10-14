1e16b0965071f37b30258f2b3cb89b9169a3cf7b
jchen4520 <jchenusa8@gmail.com>
2018-10-14 09:43:57 -0700

    document names

--- app/src/main/java/com/example/cole/jac/CameraActivity.java
+++ app/src/main/java/com/example/cole/jac/CameraActivity.java
@@ -2,6 +2,19 @@ package com.example.cole.jac;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import java.io.FileNotFoundException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.provider.MediaStore;
 
 public class CameraActivity extends AppCompatActivity {
     @Override
@@ -13,6 +26,9 @@ public class CameraActivity extends AppCompatActivity {
         wireWidgets();
         setUpHomeScreen();
     }
+
+
+
 
     private void wireWidgets(){
 
