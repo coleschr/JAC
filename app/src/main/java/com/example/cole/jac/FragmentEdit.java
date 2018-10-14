c7261a4cf8bb5b3a46ccd93639d7293c040f7ce4
cole <coleschr@usc.edu>
2018-10-13 21:48:54 -0700

    failed camera attempt

--- app/src/main/java/com/example/cole/jac/FragmentEdit.java
+++ app/src/main/java/com/example/cole/jac/FragmentEdit.java
@@ -1,6 +1,62 @@
 package com.example.cole.jac;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
-public class FragmentEdit extends Fragment {
+public class FragmentEdit extends Fragment implements View.OnClickListener {
+    private ImageView done;
+    private Fragment currentFragment;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.edit_fragment, container, false);
+
+        //wire any widgets -- must use rootView.findViewById
+        wireWidgets(rootView);
+        setOnClickListeners();
+
+        //get any other initial set up done
+
+        //Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        //startActivity(intent);
+
+        //return the view that we inflated
+        return rootView;
+    }
+
+    private void wireWidgets(View rootView) {
+        done = rootView.findViewById(R.id.imageButton_check);
+    }
+
+    private void setOnClickListeners() {
+        done.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.imageButton_check:
+                currentFragment = new FragmentHome();
+                switchToNewScreen();
+                break;
+        }
+    }
+
+    private void switchToNewScreen() {
+        //tell the fragment manager that if our current fragment isn't null, to replace whatever is there with it
+        FragmentManager fm = getFragmentManager();
+        if (currentFragment != null) {
+            fm.beginTransaction()
+                    .replace(R.id.fragment_container, currentFragment)
+                    .commit();
+        }
+    }
 }
